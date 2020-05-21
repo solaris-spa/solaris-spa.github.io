175fd4684d154d036cd5d2daf489f1a890cc4991
Solaris <info@solaris-spa.com>
2020-05-21 03:30:34 +0200

    Smooth scrolling fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// jQuery to collapse the navbar on scroll
+// Narrow the navbar on scroll
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar").addClass("navbar-narrow");
@@ -7,50 +7,42 @@ $(window).scroll(function() {
     }
 });
 
-
-
-// Select all links with hashes
-$('a[href*="#"]')
-// Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-        // On-page links
-        if (
-            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-            &&
-            location.hostname == this.hostname
-        ) {
-            // Figure out element to scroll to
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            // Does a scroll target exist?
-            if (target.length) {
-                // Only prevent default if animation is actually gonna happen
-                event.preventDefault();
-                // Store hash
-                var hash = this.hash;
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 1000, function() {
-                    // Callback after animation
-                    // Must change focus!
-                    var $target = $(target);
-                    $target.focus();
-                    if ($target.is(":focus")) { // Checking if the target was focused
-                        return false;
-                    } else {
-                        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                        $target.focus(); // Set focus again
-                    };
-                    // Add hash (#) to URL when done scrolling (default click behavior)
-                    window.location.hash = hash;
-                });
-            }
-        }
-    });
-
-// Closes the Responsive Menu on Menu Item Click
+// Close the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggler:visible').click();
+});
+
+// Smooth Scrolling
+// Select all links with hashes
+// Remove links that don't actually link to anything
+$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
+    // On-page links
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        && location.hostname == this.hostname) {
+        // Figure out element to scroll to
+        var theHash = this.hash
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000, function() {
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = theHash;
+                // Callback after animation
+                // Must change focus!
+                var $target = $(target);
+                $target.focus();
+                if ($target.is(":focus")) { // Checking if the target was focused
+                    return false;
+                } else {
+                    $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                    $target.focus(); // Set focus again
+                };
+            });
+        }
+    }
 });
