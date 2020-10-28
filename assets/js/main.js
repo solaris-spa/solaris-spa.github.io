0c951eec8e8cc8f54eb0678d839a6bd00ea70d9d
Solaris <info@solaris-spa.com>
2020-10-28 16:18:14 +0100

    Analytics # fragment tracking

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,46 @@
 // Narrow the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar").addClass("navbar-narrow");
+$(window).scroll(function () {
+    var nb = $(".navbar")
+    if (nb.offset().top > 50) {
+        nb.addClass("navbar-narrow");
     } else {
-        $(".navbar").removeClass("navbar-narrow");
+        nb.removeClass("navbar-narrow");
     }
 });
 
 // Close the Responsive Menu on Menu Item Click
-$(".navbar-collapse ul li a").click(function() {
+$(".navbar-collapse ul li a").click(function () {
     $(".navbar-toggler:visible").click();
 });
 
 // Smooth Scrolling
 // Select all links with hashes
 // Remove links that don't actually link to anything
-$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
+$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (event) {
     // On-page links
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-        && location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
+        && location.hostname === this.hostname) {
         // Figure out element to scroll to
-        var theHash = this.hash
+        var theHash = this.hash;
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
         // Does a scroll target exist?
         if (target.length) {
             // Only prevent default if animation is actually gonna happen
             event.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1000, function() {
+            $('html, body').animate({scrollTop: target.offset().top}, 1000).promise().then(function () {
                 // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = theHash;
+
+                // Google analytics # tracking
+                if (typeof gtag === "function") {
+                    gtag('config', '{{ site.google_analytics }}', {
+                        'page_title': document.title,
+                        'page_path': document.URL
+                    });
+                }
+                console.log(document.URL);
+
                 // Callback after animation
                 // Must change focus!
                 var $target = $(target);
@@ -39,9 +48,9 @@ $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
                 if ($target.is(":focus")) { // Checking if the target was focused
                     return false;
                 } else {
-                    $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                    $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
                     $target.focus(); // Set focus again
-                };
+                }
             });
         }
     }
